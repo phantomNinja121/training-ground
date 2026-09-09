8b368955f388deb03d62c5d2416820787469cfff
phantomNinja121 <pmilani@ehshouston.org>
2026-09-09 18:43:16 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// Firebase config for Training Day Master. Not a secret; the family code is what protects the data.
+window.firebaseConfig = {
+  apiKey: "AIzaSyAUtZrsqWPAcGahJx8CvWnlAki_D3qJ9zk",
+  authDomain: "training-day-master.firebaseapp.com",
+  projectId: "training-day-master",
+  storageBucket: "training-day-master.firebasestorage.app",
+  messagingSenderId: "404532882041",
+  appId: "1:404532882041:web:c6e03c38e54bffc12002a8"
+};
